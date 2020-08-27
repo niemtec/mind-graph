153f5f1cebbf0cc084ad8e34eeec353d19d65476
Jakub Niemiec <janiemiec20@gmail.com>
2020-08-27 22:44:41 +0100

    structural updates

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,21 @@
+const path = require("path");
+
+require("dotenv").config({
+  path: `.env`,
+});
+
 module.exports = {
   pathPrefix: `/mind-graph`,
+  siteMetadata: {
+    title: `Mind Graph`,
+  },
   plugins: [
     {
       resolve: `gatsby-theme-garden`,
       options: {
+        rootNote: `/hello`,
         contentPath: `${__dirname}/atoms`,
-        rootNote: `/hello`
       },
     },
   ],
-  siteMetadata: {
-    title: "Mind Graph"
-  },
 }
